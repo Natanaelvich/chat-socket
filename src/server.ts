c032d35e4df88e5c8758a839fd03739c7dc43a09
Natanael Lima <taelima1997@gmail.com>
2021-11-11 00:09:06 -0300

    add socket io initialization

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,19 @@
-import express from 'express'
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import path from 'path'
 
-const app = express()
+const app = express();
+
+const server = createServer(app);
+
+app.use(express.static(path.join(__dirname, "..", "public")))
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket);
+});
 
 
-app.listen(3000, () => console.log('server is running'))
+server.listen(3000, () => console.log("server is running"));
