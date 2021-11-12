e0cef533c71519c5847e9da90aff61801e372655
Natanael Lima <taelima1997@gmail.com>
2021-11-11 23:47:44 -0300

    add mongo connect

--- src/http.ts
+++ src/http.ts
@@ -2,17 +2,15 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import path from 'path'
-
+import mongoose  from "mongoose";
 const app = express();
 
 const server = createServer(app);
 
+mongoose.connect("mongodb://localhost/chat").then(()=>console.log('mongo connected'))
+
 app.use(express.static(path.join(__dirname, "..", "public")))
 
 const io = new Server(server);
-
-io.on("connection", (socket) => {
-  console.log("a user connected", socket);
-});
 
 export {server, io}
