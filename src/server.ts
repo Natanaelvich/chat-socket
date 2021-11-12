255bbb54154f239c8570082d80a8ab02f6541fdf
Natanael Lima <taelima1997@gmail.com>
2021-11-11 23:42:32 -0300

    refact server

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,4 @@
-import express from "express";
-import { createServer } from "http";
-import { Server } from "socket.io";
-import path from 'path'
-
-const app = express();
-
-const server = createServer(app);
-
-app.use(express.static(path.join(__dirname, "..", "public")))
-
-const io = new Server(server);
-
-io.on("connection", (socket) => {
-  console.log("a user connected", socket);
-});
-
+import { server } from "./http";
+import './websocket/ChatService'
 
 server.listen(3000, () => console.log("server is running"));
