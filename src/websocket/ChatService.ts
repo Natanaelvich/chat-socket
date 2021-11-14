fcd6f0cbf95e7deba5667cc955dc2d96fa748642
Natanael Lima <taelima1997@gmail.com>
2021-11-13 23:52:58 -0300

    add socket.on "start" in service

--- src/websocket/ChatService.ts
+++ src/websocket/ChatService.ts
@@ -1,5 +1,16 @@
+import { container } from "tsyringe";
 import { io } from "../http";
+import { CreateUserService } from "../services/CreateUserService";
 
 
 io.on("connection", (socket) => {
+
+    socket.on("start", async data => {
+        const {email, avatar, name}  = data
+        const createUserService = container.resolve(CreateUserService)
+
+        await createUserService.execute({
+            email, avatar, name,socket_id : socket.id
+        })
+    })
 });
