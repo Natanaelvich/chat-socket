b0d96c06df02a6d95a93ddc8696a263a2e004d04
Natanael Lima <taelima1997@gmail.com>
2021-11-14 00:13:39 -0300

    add events new users and get users

--- src/websocket/ChatService.ts
+++ src/websocket/ChatService.ts
@@ -1,6 +1,7 @@
 import { container } from "tsyringe";
 import { io } from "../http";
 import { CreateUserService } from "../services/CreateUserService";
+import { GetAllUsersService } from "../services/GetAllUsersService";
 
 
 io.on("connection", (socket) => {
@@ -9,8 +10,17 @@ io.on("connection", (socket) => {
         const {email, avatar, name}  = data
         const createUserService = container.resolve(CreateUserService)
 
-        await createUserService.execute({
+        const user = await createUserService.execute({
             email, avatar, name,socket_id : socket.id
         })
+
+        socket.broadcast.emit("new_users", user)
+    })
+
+    socket.on("get_users", async (callback) => {
+        const getAllUsersService = container.resolve(GetAllUsersService)
+        const users = await getAllUsersService.execute()
+
+        callback(users)
     })
 });
