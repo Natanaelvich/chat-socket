79f54bcc41cf94da914ded753d49b982124d0b15
Natanael Lima <taelima1997@gmail.com>
2021-11-18 23:55:51 -0300

    add initial on and emit message

--- src/websocket/ChatService.ts
+++ src/websocket/ChatService.ts
@@ -1,6 +1,7 @@
 import { container } from "tsyringe";
 import { io } from "../http";
 import { CreateChatRoomService } from "../services/CreateChatRoomService";
+import { CreateMessageService } from "../services/CreateMessageService";
 import { CreateUserService } from "../services/CreateUserService";
 import { GetAllUsersService } from "../services/GetAllUsersService";
 import { GetChatRoomBUsersService } from "../services/GetChatRoomBUsersService";
@@ -47,6 +48,27 @@ io.on("connection", (socket) => {
       });
     }
 
+    socket.join(room.idChatRoom)
+
     callback({ room });
   });
+
+  socket.on("message", async (data) => {
+    const getUserBySocketIdService = container.resolve(GetUserBySocketIdService);
+    const createMessageService = container.resolve(CreateMessageService);
+  
+    const user = await getUserBySocketIdService.execute(socket.id);
+
+    const message = await createMessageService.execute({
+        to : user._id,
+        text : data.message,
+        roomId : data.idChatRoom
+    })
+
+    io.to(data.idChatRoom).emit("message", {
+        message,
+        user
+    })
+  });
 });
+
